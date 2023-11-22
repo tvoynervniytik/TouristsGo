baa010297d80bab15a2f72bd7b8c57f2380df1ac
Анастасия Андриянова <nastena.0536@gmai.com>
2023-11-23 01:41:00 +0300

    1) Реализовано разграничение пользователей 2) При нажатии конпки авторизации при незаполненных кнопках выводится сообщение 3) При успешной авторизации выводится сообщение о должности сотрудника

--- TouristsGo/Pages/AuthorizationPage.xaml.cs
+++ TouristsGo/Pages/AuthorizationPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TouristsGo.DB;
 
 namespace TouristsGo.Pages
 {
@@ -20,6 +22,9 @@ namespace TouristsGo.Pages
     /// </summary>
     public partial class AuthorizationPage : Page
     {
+        public static List<Employee> employees { get; set; }
+        public static List<Post> posts { get; set; }
+        public static Employee currentUser;
         public AuthorizationPage()
         {
             InitializeComponent();
@@ -27,12 +32,40 @@ namespace TouristsGo.Pages
 
         private void loginBtn_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new AdminHomePage());
-        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new TestPage());
+
+            string login = loginTb.Text.Trim();
+            string password = passwordTb.Password.Trim();
+
+            employees = new List<Employee>(DBConnection.TouristsGo.Employee.ToList());
+            Employee currentUser = employees.FirstOrDefault(i => i.Login == login && i.Password == password);
+
+            if (currentUser != null)
+            {
+                posts = new List<Post>(DBConnection.TouristsGo.Post.ToList());
+                Post curpost = posts.FirstOrDefault(i => i.Id == currentUser.IdPost);
+                string post = curpost.Name;
+                if (currentUser != null)
+                {
+                    MessageBox.Show(post);
+                    if (currentUser.IdPost == 11)
+                    {
+                        NavigationService.Navigate(new AdminHomePage());
+                    }
+                    if (currentUser.IdPost == 12)
+                    {
+                        NavigationService.Navigate(new EngineerHomePage());
+                    }
+                }
+                else
+                    MessageBox.Show("All's wrong");
+            }
+            else
+                MessageBox.Show("!!!Заполни данные, АЛО!!!");
         }
-    }
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                NavigationService.Navigate(new TestPage());
+            }
+        }
 }
